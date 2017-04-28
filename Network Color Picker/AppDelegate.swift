b5898bfce104dd668d3a7c194365460d5373f4a2
Jon Colverson <github@vcxz.co.uk>
2017-04-28 05:07:25 +0100

    Removed template cruft

--- Network Color Picker/AppDelegate.swift
+++ Network Color Picker/AppDelegate.swift
@@ -1,26 +1,5 @@
-//
-//  AppDelegate.swift
-//  Network Color Picker
-//
-//  Created by Jon Colverson on 2017-04-28.
-//  Copyright © 2017 Jon Colverson. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
--- Network Color Picker/ViewController.swift
+++ Network Color Picker/ViewController.swift
@@ -1,27 +1,4 @@
-//
-//  ViewController.swift
-//  Network Color Picker
-//
-//  Created by Jon Colverson on 2017-04-28.
-//  Copyright © 2017 Jon Colverson. All rights reserved.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
