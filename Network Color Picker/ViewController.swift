32c3686a514473a510c297a61a1b3dd9403f964e
Jon Colverson <github@vcxz.co.uk>
2017-04-28 07:57:38 +0100

    Made progress towards sending the color to the connected peers when appropriate

--- Network Color Picker/ViewController.swift
+++ Network Color Picker/ViewController.swift
@@ -3,12 +3,36 @@ import MultipeerConnectivity
 
 class ViewController: NSViewController, MCNearbyServiceAdvertiserDelegate, MCSessionDelegate {
     
+    var color = NSColor.magenta
+    
+    @IBOutlet weak var colorWell: NSColorWell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         serviceAdvertiser = MCNearbyServiceAdvertiser(peer: MCPeerID(displayName: "Server"), discoveryInfo: nil, serviceType: "colorpicker")
         serviceAdvertiser.delegate = self
         serviceAdvertiser.startAdvertisingPeer()
+        
+        colorWell.addObserver(self, forKeyPath: #keyPath(NSColorWell.color), options: [.initial], context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        color = colorWell.color
+        print("Color changed to: \(color)") // FIXME remove
+        
+        for session in sessions {
+            sendColor(to: session.connectedPeers, of: session)
+        }
+    }
+    
+    func sendColor(to peers: [MCPeerID], of session: MCSession) {
+        try? session.send(data(from: color), toPeers: peers, with: .reliable)
+    }
+    
+    func data(from color: NSColor) -> Data {
+        // FIXME Serialize for real instead of:
+        return Data()
     }
     
     private var serviceAdvertiser: MCNearbyServiceAdvertiser!
@@ -35,7 +59,9 @@ class ViewController: NSViewController, MCNearbyServiceAdvertiserDelegate, MCSes
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
         if state == .connected {
-            // FIXME send current color
+            DispatchQueue.main.async {
+                self.sendColor(to: [peerID], of: session)
+            }
         } else if state == .notConnected {
             DispatchQueue.main.async {
                 self.sessions.remove(at: self.sessions.index(of: session)!)
